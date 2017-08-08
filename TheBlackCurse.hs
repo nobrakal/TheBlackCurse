25be913d2264af92b4d75c09abc02bebd123be8f
Alexandre Moine <nobrakal@gmail.com>
2017-08-08 21:50:42 +0200

    Can expand window

--- TheBlackCurse.hs
+++ TheBlackCurse.hs
@@ -4,8 +4,8 @@ import Data.Ratio
 
 -- NOTE: Curses is a wrapper for IO
 
-msgWin_ratio :: Rational
-msgWin_ratio = (20%100)
+msgWin_height :: Integer
+msgWin_height = 5
 
 main :: IO ()
 main =
@@ -16,13 +16,12 @@ main =
 
     y_x_width <- screenSize
 
-    let msgWin_height =  div (((fst y_x_width) * numerator msgWin_ratio))  (denominator msgWin_ratio)
+    updateBorders stdscr y_x_width
 
-    msgWin <- newWindow (msgWin_height - 2) ((snd y_x_width)-2) 1 1-- msg window
-    mainWin <- newWindow ((fst y_x_width) - msgWin_height-2) ((snd y_x_width)-2) (msgWin_height+1) 1 -- bottom window
-
-    makeBorders stdscr (0,0) (msgWin_height) (snd y_x_width) -- Make borders of msgWin
-    makeBorders stdscr (msgWin_height,0) ((fst y_x_width) - msgWin_height-1) (snd y_x_width) -- Make borders of mainWin
+    let msdim = calculateMsgWinSize y_x_width
+    msgWin <- newWindow (fst msdim) (snd msdim) 1 1 -- msg window
+    let mwdim = calculateMainWinSize y_x_width
+    mainWin <- newWindow (fst mwdim) (snd mwdim) (msgWin_height+1) 1 -- bottom window
 
     render
 
@@ -34,22 +33,33 @@ mainLoop stdscr mainWin msgWin = do
 
   if (inp == (Just (EventCharacter 'q'))) || (inp == (Just (EventCharacter 'Q'))) || (inp == (Just (EventCharacter '\ESC'))) then
    return ()
-  else
-    useInput mainWin msgWin inp >>
-    render >>
+  else do
+    useInput mainWin msgWin inp
+    render
     mainLoop stdscr mainWin msgWin
 
 
 useInput :: Window -> Window -> Maybe Event -> Curses ()
-useInput mainWin msgWwin (Just (EventSpecialKey s))
-  | (s==KeyUpArrow) || (s==KeyDownArrow) || (s==KeyLeftArrow) || (s==KeyRightArrow) = drawMsg msgWwin "A direction was pressed" -- moveCharacter s
-useInput mainWin msgWwin (Just (EventUnknown s)) = drawMsg msgWwin $ "ERROR WITH EVENT" ++ show s
---useInput mainWin msgWwin (Just (EventResized) = resize mainWin
-useInput mainWin msgWwin s = drawMsg msgWwin (show s)
+useInput mainWin msgWin (Just (EventSpecialKey s))
+  | (s==KeyUpArrow) || (s==KeyDownArrow) || (s==KeyLeftArrow) || (s==KeyRightArrow) = drawClearMsg msgWin "A direction was pressed" -- moveCharacter s
+useInput mainWin msgWin (Just (EventUnknown s)) = drawClearMsg msgWin $ "ERROR WITH EVENT" ++ show s
+useInput mainWin msgWin (Just (EventResized)) = do
+  y_x_width <- screenSize
+  stdscr <- defaultWindow
+  let msdim = calculateMsgWinSize y_x_width
+  let mwdim = calculateMainWinSize y_x_width
+  updateWindow msgWin $ resizeWindow (fst msdim) (snd msdim)
+  updateWindow mainWin $ moveWindow (fst msdim) 1
+  updateWindow mainWin $ resizeWindow (fst mwdim) (snd mwdim)
+  updateBorders stdscr y_x_width
 
--- Draw a message on the msg window (clear all before)
-drawMsg :: Window -> String -> Curses ()
-drawMsg msgWin str = updateWindow msgWin $ clear >> (drawString str)
+useInput mainWin msgWin s = drawClearMsg msgWin (show s)
+
+-- Draw a message on the window (clear all before)
+drawClearMsg :: Window -> String -> Curses ()
+drawClearMsg win str = updateWindow win $ do
+  clear
+  drawString str
 
 -- Draw borders of a rectangle starting at (pos_x,pos_y) with y rows and x columns on win
 makeBorders :: Window -> (Integer,Integer) -> Integer -> Integer -> Curses ()
@@ -70,3 +80,17 @@ makeBorders win (pos_y,pos_x) y x = updateWindow win $ do
   moveCursor (pos_y+1) (pos_x+x-1)
   drawLineV (Just glyphLineV) $ y-2 -- Vertical Right line
   moveCursor pos_y pos_x -- Reset cursor pos
+
+calculateMainWinSize :: (Integer, Integer) -> (Integer, Integer)
+calculateMainWinSize y_x_width = (((fst y_x_width) - msgWin_height-2),((snd y_x_width)-2))
+
+calculateMsgWinSize :: (Integer, Integer) -> (Integer, Integer)
+calculateMsgWinSize y_x_width = ((msgWin_height - 2),((snd y_x_width)-2))
+
+updateBorders :: Window -> (Integer,Integer) -> Curses ()
+updateBorders stdscr y_x_width = do
+  updateWindow stdscr clear
+  let msdim = calculateMsgWinSize y_x_width
+  let mwdim = calculateMainWinSize y_x_width
+  makeBorders stdscr (0,0) ((fst msdim) +2) ((snd msdim)+2) -- Make borders of msgWin
+  makeBorders stdscr (msgWin_height,0) ((fst mwdim) +1) ((snd mwdim)+2) -- Make borders of mainWin
