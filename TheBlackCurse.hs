30d7b0b88f45d1c0411cce6f78bc553bd176ce5b
Alexandre Moine <nobrakal@gmail.com>
2017-08-08 16:45:17 +0200

    Switch to ncurses

--- TheBlackCurse.hs
+++ TheBlackCurse.hs
@@ -1,43 +1,73 @@
-import UI.HSCurses.Curses
-import UI.HSCurses.CursesHelper
+import UI.NCurses
 import System.Exit
 
+-- NOTE: Curses is a wrapper for IO
+
 main :: IO ()
-main = do
-  initScr --Start
-  echo False -- Disable echo
+main =
+  runCurses $ do --Start
+    setEcho False -- Disable echo
+    setCursorMode CursorInvisible -- No more cursor
+    stdscr <- defaultWindow
 
-  let msgwin_width = 5
-  x_y_width <- scrSize
-  mainwin <- newWin ((fst x_y_width) - msgwin_width) 0 msgwin_width 0
-  msgwin <- newWin msgwin_width 0 0 0
+    let msgWin_width = 5
+    y_x_width <- screenSize
 
-  wMove mainwin 1 1
-  wMove msgwin 1 1
+    msgWin <- newWindow (msgWin_width - 2) ((snd y_x_width)-2) 1 1-- msg window
+    mainWin <- newWindow ((fst y_x_width) - msgWin_width-2) ((snd y_x_width)-2) (msgWin_width+1) 1 -- bottom window
 
-  mainLoop mainwin msgwin
+    --makeBorders mainWin -- draw the default borders
+    --makeBorders msgWin
+    makeBorders stdscr (0,0) (msgWin_width) (snd y_x_width) -- Make borders of msgWin
+    makeBorders stdscr (msgWin_width,0) ((fst y_x_width) - msgWin_width-1) (snd y_x_width) -- Make borders of mainWin
 
-mainLoop :: Window -> Window -> IO ()
-mainLoop mainwin msgwin = do
-  inp <- getCh
+    --updateWindow stdscr $ setTouched False -- DO NOT UPDATE stdscr, it will blank evrything
 
-  werase msgwin
-  werase mainwin
+    render
 
-  useInput msgwin inp
+    {- getEvent stdscr Nothing
+    return () -}
+    mainLoop mainWin msgWin
 
-  refreshWindow mainwin
-  refreshWindow msgwin
+mainLoop :: Window -> Window -> Curses ()
+mainLoop mainWin msgWin = do
+  stdscr <- defaultWindow
+  inp <- getEvent stdscr Nothing
 
-  mainLoop mainwin msgwin
+  if (inp == (Just (EventCharacter 'q'))) || (inp == (Just (EventCharacter 'Q'))) || (inp == (Just (EventCharacter 'q'))) || (inp == (Just (EventCharacter '\ESC'))) then
+   return ()
+  else
+    useInput msgWin inp >>
+    render >>
+    mainLoop mainWin msgWin
 
-useInput :: Window -> Key -> IO()
-useInput msgwin s
-  | s == KeyChar '\ESC' = (endWin >> exitSuccess) -- Exit when ESC is pressed
-  | elem (head (displayKey s)) ['q','s','z','d'] = mvWAddStr msgwin 1 1 "A direction was pressed" -- moveCharacter s
-  | otherwise = mvWAddStr msgwin 1 1 $ displayKey s
 
-refreshWindow :: Window -> IO()
-refreshWindow win = do
-  wBorder win defaultBorder
-  wRefresh win
+useInput :: Window -> Maybe Event -> Curses ()
+useInput win (Just (EventSpecialKey s))
+  | (s==KeyUpArrow) || (s==KeyDownArrow) || (s==KeyLeftArrow) || (s==KeyRightArrow) = drawMsg win "A direction was pressed" -- moveCharacter s
+useInput win (Just (EventUnknown s)) = drawMsg win "ERROR WITH EVENT"
+useInput win s = drawMsg win (show s)
+
+-- Draw a message on the msg window (clear all before)
+drawMsg :: Window -> String -> Curses ()
+drawMsg msgWin str = updateWindow msgWin $ clear >> (drawString str)
+
+-- Draw borders of a rectangle starting at (pos_x,pos_y) with y rows and x columns on win
+makeBorders :: Window -> (Integer,Integer) -> Integer -> Integer -> Curses ()
+makeBorders win (pos_y,pos_x) y x = updateWindow win $ do
+  moveCursor pos_y pos_x
+  drawGlyph glyphCornerUL
+  drawLineH (Just glyphLineH) $ x-2 -- Horizontal top line
+  moveCursor (pos_y+1) pos_x
+  drawLineV (Just glyphLineV) $ y-2 -- Vertical left line
+  moveCursor (pos_y+y-1) pos_x
+  drawGlyph glyphCornerLL
+  moveCursor (pos_y+y-1) (pos_x+1)
+  drawLineH (Just glyphLineH) $ x-2 -- Horizontal bottom line
+  moveCursor (pos_y+y-1) (pos_x+x-1)
+  drawGlyph glyphCornerLR
+  moveCursor (pos_y) (pos_x+x-1)
+  drawGlyph glyphCornerUR
+  moveCursor (pos_y+1) (pos_x+x-1)
+  drawLineV (Just glyphLineV) $ y-2 -- Vertical Right line
+  moveCursor pos_y pos_x -- Reset cursor pos
