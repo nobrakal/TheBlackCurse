894dfbf04ba16a73ff86f262a0753e7cc8a6260a
Alexandre Moine <nobrakal@gmail.com>
2017-08-10 10:27:29 +0200

    Add State

--- src/TheBlackCurse.hs
+++ src/TheBlackCurse.hs
@@ -13,7 +13,11 @@ data Game = Game {
   stdscr :: Window,
   mainWin :: Window,
   msgWin :: Window,
-  m :: LevelMap,
+  m :: LevelMap
+}
+
+data State = State {
+  game :: Game,
   todo :: Maybe (Curses ())
 }
 
@@ -38,31 +42,31 @@ main = do
     drawTab mainWin $ getCurrentDisplay (levelMap map1) (0,0) (mwdim)
     render
 
-    mainLoop (Game stdscr mainWin msgWin map1 $ Just $ drawClearMsg msgWin "Welcome") -- Run mainLoop
+    mainLoop (State (Game stdscr mainWin msgWin map1) $ Just $ drawClearMsg msgWin "Welcome") -- Run mainLoop
 
-mainLoop :: Game -> Curses ()
-mainLoop (Game stdscr mainWin msgWin map1 (Just todo)) = do
+mainLoop :: State -> Curses ()
+mainLoop (State game (Just todo))= do
   todo
   render
-  inp <- getEvent stdscr Nothing
-  mainLoop (useInput (Game stdscr mainWin msgWin map1 (Just todo)) inp)
+  inp <- getEvent (stdscr game) Nothing
+  mainLoop (useInput game inp)
 
-mainLoop (Game stdscr mainWin msgWin map1 Nothing) = return ()
+mainLoop (State _ Nothing) = return ()
 
-useInput :: Game -> Maybe Event -> Game
-useInput (Game stdscr mainWin msgWin map1 todo) (Just (EventCharacter c))
-  | c=='Q' || c=='q' || c=='\ESC' = (Game stdscr mainWin msgWin map1 Nothing)
-useInput (Game stdscr mainWin msgWin (LevelMap map1 (cx, cy)) todo) (Just (EventSpecialKey s)) -- BUG Ajouter tests sur positions
-  | (s==KeyUpArrow)= (Game stdscr mainWin msgWin (LevelMap map1 (cx-1, cy)) (Just (screenSize >>= moveCamera mainWin (LevelMap map1 (cx, cy)) (-1,0))))
-  | (s==KeyDownArrow)= (Game stdscr mainWin msgWin (LevelMap map1 (cx+1, cy)) (Just (screenSize >>= moveCamera mainWin (LevelMap map1 (cx, cy)) (1,0))))
-  | (s==KeyLeftArrow)= (Game stdscr mainWin msgWin (LevelMap map1 (cx, cy-1)) (Just (screenSize >>= moveCamera mainWin (LevelMap map1 (cx, cy)) (0,-1))))
-  | (s==KeyRightArrow)= (Game stdscr mainWin msgWin (LevelMap map1 (cx, cy+1)) (Just (screenSize >>= moveCamera mainWin (LevelMap map1 (cx, cy)) (0,1))))
-useInput (Game stdscr mainWin msgWin map1 todo) (Just (EventResized)) = (Game stdscr mainWin msgWin map1 $ Just $ updateScreenSize (Game stdscr mainWin msgWin map1 todo))
-useInput (Game stdscr mainWin msgWin map1 todo) (Just (EventUnknown s)) = (Game stdscr mainWin msgWin map1 $ Just $ drawClearMsg msgWin $"ERROR WITH EVENT" ++ show s )  -- ERROR
-useInput (Game stdscr mainWin msgWin map1 todo) s = (Game stdscr mainWin msgWin map1 $ Just $ drawClearMsg msgWin (show s))  -- Any other input
+useInput :: Game -> Maybe Event -> State
+useInput game (Just (EventCharacter c))
+  | c=='Q' || c=='q' || c=='\ESC' = State game Nothing
+useInput (Game stdscr mainWin msgWin (LevelMap map1 (cx, cy))) (Just (EventSpecialKey s)) -- BUG Ajouter tests sur positions
+  | (s==KeyUpArrow)= State (Game stdscr mainWin msgWin (LevelMap map1 (cx-1, cy))) (Just (moveCamera mainWin (LevelMap map1 (cx, cy)) (-1,0)))
+  | (s==KeyDownArrow)= State (Game stdscr mainWin msgWin (LevelMap map1 (cx+1, cy))) (Just (moveCamera mainWin (LevelMap map1 (cx, cy)) (1,0)))
+  | (s==KeyLeftArrow)= State (Game stdscr mainWin msgWin (LevelMap map1 (cx, cy-1))) (Just (moveCamera mainWin (LevelMap map1 (cx, cy)) (0,-1)))
+  | (s==KeyRightArrow)= State (Game stdscr mainWin msgWin (LevelMap map1 (cx, cy+1))) (Just (moveCamera mainWin (LevelMap map1 (cx, cy)) (0,1)))
+useInput game (Just (EventResized)) = State game $ Just $ updateScreenSize game
+useInput game (Just (EventUnknown s)) = State game $ Just $ drawClearMsg (msgWin game) $"ERROR WITH EVENT" ++ show s  -- ERROR
+useInput game s = State game $ Just $ drawClearMsg (msgWin game) (show s)  -- Any other input
 
 updateScreenSize :: Game -> Curses ()
-updateScreenSize (Game stdscr mainWin msgWin map1 todo) =  do
+updateScreenSize (Game stdscr mainWin msgWin map1) =  do
   y_x_width <- screenSize
   updateWindow mainWin clear
   let msdim = calculateMsgWinSize y_x_width
@@ -88,5 +92,5 @@ updateBorders stdscr y_x_width = do
   makeBorders stdscr (msgWin_height,0) ((fst mwdim) +2) ((snd mwdim)+2) -- Make borders of mainWin
 
   -- Move the camera if possible
-moveCamera :: Window -> LevelMap -> (Integer,Integer) -> (Integer,Integer) -> Curses()
-moveCamera win (LevelMap map1 (cy,cx)) (y,x) y_x_stdscr = drawTab win $ getCurrentDisplay map1 ((y+cy),(x+cx)) (calculateMainWinSize y_x_stdscr)
+moveCamera :: Window -> LevelMap -> (Integer,Integer) -> Curses()
+moveCamera win (LevelMap map1 (cy,cx)) (y,x) = screenSize >>= \arg -> (drawTab win $ getCurrentDisplay map1 ((y+cy),(x+cx)) (calculateMainWinSize arg))
